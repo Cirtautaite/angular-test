62709fa5b170a4d6db5ea7e44472186d0f1aa930
Modesta Cirtautaite <m.cirtautaite@gmail.com>
2020-09-26 12:09:15 +0300

    Trying to create single page

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -10,5 +10,6 @@ export const routes: Routes =[
     {path: 'home', component: HomeComponent},
     {path: 'menu', component: MenuComponent},
     {path: 'about', component: AboutComponent},
+    {path: 'contactus', component: ContactComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
